8211551fd635b82e3856a922079e55c81a121cf0
AssoDePicche <samuelprado730@gmail.com>
2023-06-02 21:09:05 -0300

    feat: add binary search

---
+++ src/BinarySearch.ts
@@ -0,0 +1,21 @@
+export default function binarySearch(haystack: number[], needle: number): boolean {
+  let low = 0;
+
+  let high = haystack.length;
+
+  do {
+    let middle = Math.floor(low + (high - low) / 2);
+
+    let value = haystack[middle];
+
+    if (value === needle) {
+      return true;
+    } else if (needle > value) {
+      low = middle + 1;
+    } else {
+      high = middle;
+    }
+  } while (low < high);
+
+  return false;
+}
