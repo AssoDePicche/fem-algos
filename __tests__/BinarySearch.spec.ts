68b635871e3b659a4582194f981ba57b7feaa431
AssoDePicche <samuelprado730@gmail.com>
2023-06-02 21:09:17 -0300

    test: add binary search

---
+++ __tests__/BinarySearch.spec.ts
@@ -0,0 +1,17 @@
+import binarySearch from "../src/BinarySearch";
+
+test('binary search array', () => {
+   const list = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+    
+    expect(binarySearch(list, 69)).toEqual(true);
+
+    expect(binarySearch(list, 1336)).toEqual(false);
+
+    expect(binarySearch(list, 69420)).toEqual(true);
+
+    expect(binarySearch(list, 69421)).toEqual(false);
+
+    expect(binarySearch(list, 1)).toEqual(true);
+
+    expect(binarySearch(list, 0)).toEqual(false);
+});
