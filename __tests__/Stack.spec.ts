a633aed85edbe35390794497907749447d867a33
AssoDePicche <samuelprado730@gmail.com>
2023-06-03 14:17:32 -0300

    test: add stack

---
+++ __tests__/Stack.spec.ts
@@ -0,0 +1,33 @@
+import Stack from "../src/Stack";
+
+test("Stack", () => {
+  const list = new Stack<number>();
+
+  list.push(5);
+
+  list.push(7);
+
+  list.push(9);
+
+  expect(list.pop()).toEqual(9);
+
+  expect(list.size()).toEqual(2);
+
+  list.push(11);
+
+  expect(list.pop()).toEqual(11);
+
+  expect(list.pop()).toEqual(7);
+
+  expect(list.peek()).toEqual(5);
+
+  expect(list.pop()).toEqual(5);
+
+  expect(list.pop()).toEqual(undefined);
+
+  list.push(69);
+
+  expect(list.peek()).toEqual(69);
+
+  expect(list.size()).toEqual(1);
+});
